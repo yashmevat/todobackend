f1bcd3bf0ffd36f612e9aa51fe4728b22024ad32
yashmevat <145139616+yashmevat@users.noreply.github.com>
2025-10-16 17:15:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,14 +12,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use("/api/users", userRoutes);
-app.use("/api/notes", noteRoutes);
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
+
+
+// Routes
+app.use("/api/users", userRoutes);
+app.use("/api/notes", noteRoutes);
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
